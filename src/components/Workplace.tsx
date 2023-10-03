7eff43b72075dda7eafc2bde4c98d6a26158c911
Pratasenka <a.pratasenka@gmail.com>
2023-10-03 22:52:32 +0300

    update algorithm

--- src/components/Workplace.tsx
+++ src/components/Workplace.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-
 import { Chart } from './Chart';
-import { findWinnerCorrection, findWinnerUniform } from '../utils';
+import { findWinners } from '../utils';
 
 export interface UserData {
     id: string;
@@ -13,37 +12,31 @@ export interface UserData {
 const usersDataCorrection: UserData[] = [];
 const usersDataUniform: UserData[] = [];
 
-const wins = 100;
-const rounds = 1000;
+const wins = 10;
+const rounds = 120;
 
-for (let i = 0; i < 5; i++) {
+for (let i = 1; i < 6; i++) {
     usersDataCorrection.push({
         id: `#${i}`,
-        wins: wins * i,
+        wins: i % 2 === 0 ? 0 : wins * i,
         rounds: rounds,
     })
 
     usersDataUniform.push({
         id: `#${i}`,
-        wins: wins * i,
+        wins: i % 2 === 0 ? wins : wins * i,
         rounds: rounds,
     })
 }
 
 export const Workplace = () => {
-    for (let i = 0; i < 100000; i++) {
-        const winnerWithCorrection = findWinnerCorrection(usersDataCorrection);
+    for (let i = 0; i < 200; i++) {
+        const [winnerWithCorrection, winnerWithUniform] = findWinners(usersDataCorrection, usersDataUniform);
         ++winnerWithCorrection.wins;
-        usersDataCorrection.forEach(userData => ++userData.rounds);
-
-        const winnerWithUniform = findWinnerUniform(usersDataUniform);
         ++winnerWithUniform.wins;
-        usersDataUniform.forEach(userData => ++userData.rounds);
+        usersDataCorrection.forEach(userData => userData.rounds++);
+        usersDataUniform.forEach(userData => userData.rounds++);
     }
-
-
-    console.log(usersDataCorrection, usersDataUniform)
-
 
     const labels = usersDataCorrection.map(userData => userData.id);
     const data = {
@@ -53,21 +46,29 @@ export const Workplace = () => {
                 label: 'Uniform Distribution',
                 data: usersDataUniform.map(userData => userData.wins),
                 borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                backgroundColor: 'rgba(255, 99, 132, 0.3)',
+                borderWidth: 2,
+                tension: 0.2,
             },
             {
                 label: 'Distribution with corrections',
                 data: usersDataCorrection.map(userData => userData.wins),
                 borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                backgroundColor: 'rgba(53, 162, 235, 0.3)',
+                borderWidth: 2,
+                tension: 0.2,
             },
         ],
     };
-
+    const options = {
+    }
 
 
     return <div className="workplace">
         <h1>Volunteling Distributions Chart</h1>
-        <Chart data={data} />
+        <Chart
+            data={data}
+            options={options}
+        />
     </div>
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,92 +2,92 @@ import random from "random";
 
 import { UserData } from "./components/Workplace";
 
-interface UserDataProbability extends UserData {
-    probability: number;
-}
-
-const FIXED = 2;
-
-const getRandomArbitrary = (min: number, max: number): number => {
-    return Math.random() * (max - min) + min;
+// absolutely equal
+const calculateUsersEqualsProbabilities = (usersData: UserData[]): number[] => {
+    const userProbability = 1 / usersData.length;
+    return usersData.map((userData) => {
+        const userProbabilityCorrection = userData.wins / userData.rounds || 0;
+        return userProbability - userProbabilityCorrection >= 0
+            ? userProbability - userProbabilityCorrection
+            : 0;
+    });
 };
 
-export const getUniformRandom = random.uniform(0, 1);
+// equal with random factor
+const calculateUsersProbabilities = (usersData: UserData[]): number[] => {
+    const userProbability = 1 / usersData.length;
+    return usersData.map((userData) => {
+        let userProbabilityCorrection = userData.wins / userData.rounds || 0;
 
-const calculateUserProbabilityCorrection = (userData: UserData): number => {
-    const userWinsToRounds: number = userData.wins / userData.rounds || 0;
-    return (1 - (userWinsToRounds !== 1 ? userWinsToRounds : 0.99)) ^ 2;
-};
-
-const sortUserProbabilities = (
-    usersDataProbability: UserDataProbability[]
-): UserDataProbability[] => {
-    return usersDataProbability.sort(
-        (a: UserDataProbability, b: UserDataProbability) =>
-            a.probability - b.probability
-    );
-};
-
-export const findWinnerCorrection = (usersData: UserData[]): UserData => {
-    let usersDataProbability: UserDataProbability[] = usersData.map(
-        (userData) => {
-            const probability =
-                calculateUserProbabilityCorrection(userData) / usersData.length;
-            return {
-                ...userData,
-                probability: Number(probability.toFixed(FIXED)),
-            };
+        if (userProbabilityCorrection > userProbability)
+            return (
+                userProbability - userProbabilityCorrection * userProbability
+            );
+        if (userProbabilityCorrection < userProbability) {
+            return (
+                userProbability +
+                (1 - userProbabilityCorrection) * userProbability
+            );
         }
-    );
+        return userProbability;
+    });
+};
 
-    usersDataProbability = sortUserProbabilities(usersDataProbability);
+export const findWinnerCorrection = (
+    usersData: UserData[],
+    randomValue: number
+): UserData => {
+    const usersProbability = calculateUsersEqualsProbabilities(usersData);
+    // const usersProbability = calculateUsersProbabilities(usersData);
 
-    const sumOfUsersProbbilities: number = usersDataProbability.reduce(
-        (acc: number, curr: UserDataProbability) => acc + curr.probability,
+    const sumOfUsersProbailities: number = usersProbability.reduce(
+        (acc: number, curr: number) => acc + curr,
         0
     );
 
-    const borderedRandomValue = getRandomArbitrary(0, sumOfUsersProbbilities);
-
-    const usersProbabilityBorders: number[] = usersDataProbability.reduce(
-        (acc: number[], curr: UserDataProbability) => {
-            const topBorder = curr.probability + (acc[acc.length - 1] ?? 0);
-            acc.push(Number(topBorder.toFixed(FIXED)));
+    const usersProbabilityBorders: number[] = usersProbability.reduce(
+        (acc: number[], curr: number) => {
+            const topBorder = curr + (acc[acc.length - 1] ?? 0);
+            acc.push(Number(topBorder));
             return acc;
         },
         [] as number[]
     );
 
-    // console.log(us ersProbabilityBorders);
-
+    const borderedRandomValue = sumOfUsersProbailities * randomValue;
     const winnerIndex: number = usersProbabilityBorders.findIndex(
-        (probability) => probability >= borderedRandomValue
+        (probability) => borderedRandomValue <= probability
     );
 
-    const winner: UserData | undefined = usersData.find(
-        (userData) => userData.id === usersDataProbability[winnerIndex].id
-    );
-
-    if (winner) return winner;
-    else return findWinnerCorrection(usersData);
+    return usersData[winnerIndex];
 };
 
-export const findWinnerUniform = (usersData: UserData[]): UserData => {
+export const findWinnerUniform = (
+    usersData: UserData[],
+    randomValue: number
+): UserData => {
     const userProbability: number = 1 / usersData.length;
-
-    const borderedRandomValue = getUniformRandom();
-
     const winnerIndex: number = usersData.findIndex((userData, index) => {
-        return (
-            borderedRandomValue <=
-            Number(userProbability.toFixed(FIXED)) * (index + 1)
-        );
+        return randomValue <= Number(userProbability) * (index + 1);
     });
 
     return usersData[winnerIndex];
 };
 
+const getUniformRandom = random.uniform(0, 1);
+
+const getRandomArbitrary = (min: number, max: number): number => {
+    return Math.random() * (max - min) + min;
+};
+
 export const findWinners = (
     usersDataCorrection: UserData[],
     usersDataUniform: UserData[]
-) => {};
+) => {
+    const borderedRandomValue = getRandomArbitrary(0, 1);
+
+    return [
+        findWinnerCorrection(usersDataCorrection, borderedRandomValue),
+        findWinnerUniform(usersDataUniform, borderedRandomValue),
+    ];
+};
